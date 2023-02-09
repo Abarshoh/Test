924bb7217ca5f7c8cdcb450ef89cf82feab49cc9
Professional <akbarshohakramov1410@gmail.com>
2023-02-09 23:41:19 +0500

    test

---
+++ app/src/main/java/uz/akbar/mode/Test.kt
@@ -0,0 +1,9 @@
+package uz.akbar.mode
+
+data class Test(var question: String,
+           var answer1: String,
+           var answer2: String,
+           var answer3: String,
+           var answer4: String,
+           var correct_answer: String ) {
+}
